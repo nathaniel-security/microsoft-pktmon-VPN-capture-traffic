9bfa019b8c02c0709760b8c106ef251354b7bb04
we-host <cnathanielb007@gmail.com>
2021-06-03 19:17:44 +0530

    'update'

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os 
 import socket
-
+import time
 
 
 os.system('pktmon start --capture')
@@ -10,10 +10,11 @@ domain_name = 'google.com'
 ip_add = socket.gethostbyname(domain_name)
 
 os.system("ping " + domain_name)
-
+time.sleep(10)
 os.system('pktmon stop')
+time.sleep(10)
 os.system('pktmon etl2txt PktMon.etl')
-
+time.sleep(10)
 os.system('echo ' + ip_add + " >> ip")
 os.system("clip < ip")
 
